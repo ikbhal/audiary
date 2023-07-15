ba94620d9facbc9012f2ec75a8090d93c5dff699
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-15 18:10:31 +0530

    add, record, play, pause working, list not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,175 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:record/record.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(AudioDiaryApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class AudioDiaryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Audio Diary',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AudioDiaryScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class AudioDiaryScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _AudioDiaryScreenState createState() => _AudioDiaryScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AudioDiaryScreenState extends State<AudioDiaryScreen> {
+  List<File> _diaryEntries = [];
+  late Record audioRecord;
+  late AudioPlayer audioPlayer;
+  bool isRecording = false;
+  String audioPath = '';
+  bool isPlaying = false;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    audioPlayer = AudioPlayer();
+    audioRecord = Record();
+    _initializeDiaryEntries();
+  }
+
+  @override
+  void dispose() {
+    audioRecord.dispose();
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  Future<void> _initializeDiaryEntries() async {
+    Directory appDir = await getApplicationDocumentsDirectory();
+    List<FileSystemEntity> files = appDir.listSync();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _diaryEntries = files.whereType<File>().toList();
     });
+  }
+
+  Future<void> startRecording() async {
+    try {
+      if (await audioRecord.hasPermission()) {
+        await audioRecord.start();
+        setState(() {
+          isRecording = true;
+        });
+        // Directory appDir = await getApplicationDocumentsDirectory();
+        // String filePath = '${appDir.path}/${DateTime.now().toString()}.aac';
+      }
+    } catch (e) {
+      print("Error during recording of audio is $e");
+    }
+  }
+
+  Future<void> stopRecording() async {
+    try {
+      String? path = await audioRecord.stop();
+      setState(() {
+        isRecording = false;
+        audioPath = path!;
+      });
+    } catch (e) {
+      print("Error during stopping of recording is $e");
+    }
+  }
+
+  Future<void> _playAudio(String filePath) async {
+    // Add logic to handle playing state, UI changes, etc.
+
+  }
+
+  Future<void> _deleteAudio(String filePath) async {
+    File audioFile = File(filePath);
+    await audioFile.delete();
+
+    await _initializeDiaryEntries();
+  }
+
+  // isPlaying ? stopPlaying : startPlaying
+  Future<void> startPlaying() async {
+    try {
+      Source urlSource = UrlSource(audioPath);
+      audioPlayer.play(urlSource);
+    } catch (e) {
+      print("Unable to start playing due to $e");
+    }
+  }
+
+  Future<void> stopPlaying() async {
+    try {
+      audioPlayer.stop();
+    } catch (e) {
+      print("Unable to stop playing due to $e");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Audio Diary'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: isRecording ? stopRecording : startRecording,
+      //   child: isRecording ? const Text("stop"): const Icon(Icons.mic),
+      // ),
+
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ElevatedButton(
+            onPressed: isRecording ? stopRecording : startRecording,
+            child: Icon(isRecording ? Icons.stop : Icons.mic),
+          ),
+          SizedBox(height: 16),
+          if (!isRecording && audioPath != null)
+            ElevatedButton(
+              onPressed: isPlaying ? stopPlaying : startPlaying,
+              child: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+            )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView.builder(
+        itemCount: _diaryEntries.length,
+        itemBuilder: (context, index) {
+          File diaryEntry = _diaryEntries[index];
+
+          return ListTile(
+            title: Text(
+              '${diaryEntry.path.split('/').last}',
+            ),
+            subtitle: Text(
+              '${diaryEntry.lastModifiedSync().toString()}',
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.play_arrow),
+                  onPressed: () => _playAudio(diaryEntry.path),
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () => _deleteAudio(diaryEntry.path),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
